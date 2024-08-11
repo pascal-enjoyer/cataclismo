fe1d4dba1296f5382443e29e1f189d7d2f793428
pascal_pro_2007 <m.brednev89@gmail.com>
2024-08-11 21:14:55 +0500

    добавил инвентарь

--- Cataclismo/Assets/Scripts folder/Player/Inventory/InventoryItem.cs
+++ Cataclismo/Assets/Scripts folder/Player/Inventory/InventoryItem.cs
@@ -1,6 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(fileName = "New Inventory Item", menuName = "Inventory Item", order = 51)]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public ItemType itemType;
+    public ItemRarity itemRarity;
+    public BonusType bonusType;
+    public int bonusValue; // Значение бонуса
+    public Sprite itemIcon; // Иконка предмета
+}
 
 public enum ItemType
 {
@@ -22,15 +32,4 @@ public enum BonusType
 {
     Attack,
     Defense
-}
-
-[System.Serializable]
-public class InventoryItem
-{
-    public string itemName;
-    public ItemType itemType;
-    public ItemRarity itemRarity;
-    public BonusType bonusType;
-    public int bonusValue; // Значение бонуса
-    public Sprite itemIcon; // Иконка предмета
 }
