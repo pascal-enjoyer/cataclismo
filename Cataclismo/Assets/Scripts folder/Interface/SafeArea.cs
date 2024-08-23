40913043cfe1442c91273764e222b1e1bf9d455f
pascal-enjoyer <115283865+pascal-enjoyer@users.noreply.github.com>
2024-08-24 02:34:53 +0500

    предбилдовый сейв

---
+++ Cataclismo/Assets/Scripts folder/Interface/SafeArea.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+    private void Start()
+    {
+        var rectTransform = GetComponent<RectTransform>();
+        var safeArea = Screen.safeArea;
+        var anchorMin = safeArea.position;
+        var anchorMax = anchorMin + safeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+
+}
