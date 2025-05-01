14dbcfa21fb60ae0dd9551c25be406d7d6b20d29
pascal_pro_2007 <m.brednev89@gmail.com>
2025-05-01 11:30:54 +0500

    backup

--- Cataclismo/Assets/Scripts folder/World/GameLevelManager.cs
+++ Cataclismo/Assets/Scripts folder/World/GameLevelManager.cs
@@ -23,13 +23,13 @@ public class GameLevelManager : MonoBehaviour
     { 
         // Получаем текущий уровень из PlayerPrefs
         int currentLevel = PlayerPrefs.GetInt("CurrentLevel", 0);
-            enemyTransform.GetComponent<ActiveEnemy>().enemy = enemies[currentLevel % enemies.Length];
+        enemyTransform.GetComponent<ActiveEnemy>().enemy = enemies[currentLevel % enemies.Length];
         enemyTransform.GetComponent<ActiveEnemy>().RefreshEnemyStats();
         // Устанавливаем фон для текущего уровня
-            backgroundRenderer.sprite = levelBackgrounds[currentLevel % levelBackgrounds.Length];
+        backgroundRenderer.sprite = levelBackgrounds[currentLevel % levelBackgrounds.Length];
         // Спавним врага для текущего уровня
         GameObject enemyGO;
-            enemyGO = Instantiate(enemyPrefabs[currentLevel % enemyPrefabs.Length], enemyTransform);
+        enemyGO = Instantiate(enemyPrefabs[currentLevel % enemyPrefabs.Length], enemyTransform);
         enemyTransform.GetComponent<ActiveEnemy>().OnEnemyDied.AddListener(CompleteCurrentLevel);
         enemyTransform.GetComponent <ActiveEnemy>().EnemyGameobject = enemyGO;
         enemyTransform.GetComponent<ActiveEnemy>().OnEnemyDied.AddListener(canvas.GetComponent<LevelEndingUISpawner>().WinLevelSpawn);
